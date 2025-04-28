5fd6570e63c380207f4ae21d70a036e7bc3753d7
TorlapSayHi <torlap.ritchai@gmail.com>
2025-04-28 11:49:02 +0700

    Refactor ProductSeeder; comment out product seeding logic for future implementation

--- mylaravel/resources/views/login.blade.php
+++ mylaravel/resources/views/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    @vite('resources/css/app.css')
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen px-4 sm:px-0">
